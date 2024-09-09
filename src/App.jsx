00a135dba7f7b0d1dedccf3f106a1c983d91391a
Beasty002 <xasbin12@gmail.com>
2024-09-09 23:57:01 +0545

    add: home page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import HomePage from './components/HomePage'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header />
+
+
+        <HomePage />
+      </section>
+      <h1></h1>
+
+    </div>
+  )
+}
+
+export default App
