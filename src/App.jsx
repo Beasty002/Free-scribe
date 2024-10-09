cd0321d92bc7abfec0321cd0110af54861f1711d
Beasty002 <xasbin12@gmail.com>
2024-10-09 21:11:47 +0545

    3:35

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react'
 import HomePage from './components/HomePage'
 import Header from './components/Header'
+import FileDisplay from './components/FileDisplay'
+
+
 
 
 function App() {
+  const [file, setFile] = useState(null)
+  const [audioStream, setAudioStream] = useState(null)
+
+  const isAudioAvaialable = file || audioStream
+  function handleAudioReset() {
+    setFile(null)
+    setAudioStream(null)
+  }
 
   return (
     <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
       <section className='min-h-screen flex flex-col'>
         <Header />
-
-
-        <HomePage />
+        {isAudioAvaialable ? (<FileDisplay file={file} audioStream={setAudioStream} handleAudioReset={handleAudioReset} />) : (<HomePage setFile={setFile} setAudioStream={setAudioStream} />)}
       </section>
       <h1></h1>
 
